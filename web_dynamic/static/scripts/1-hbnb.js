8ca68596a3fa65776ec5c5e8a459b404f694beac
Soulsaw <ssawadogo292@gmail.com>
2024-01-13 10:56:24 +0000

    fix task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    let selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        updateAmenitiesList();
+    });
+
+    function updateAmenitiesList() {
+        const amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('DIV#amenities h4').text(amenitiesList);
+	    console.log(amenitiesList);
+    }
+});
+
